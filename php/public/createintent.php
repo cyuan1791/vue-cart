430cd7bf1348298c568cb56ba1451db95ecea611
Mark Yuan <mark@webasone.com>
2025-08-31 05:06:11 +0000

    updte

--- php/public/createintent.php
+++ php/public/createintent.php
@@ -4,26 +4,26 @@ require_once 'shared.php';
 header('Content-Type: application/json; charset=utf-8');
 
 $json_data = file_get_contents('php://input');
-$data = json_decode($json_data, true); 
+$data      = json_decode($json_data, true);
 
 try {
-  $paymentIntent = $stripe->paymentIntents->create([
-    'automatic_payment_methods' => ['enabled' => true],
-   'amount' => (int)($data['amount']),
-   //'amount' => 1099,
-    'currency' => 'eur',
-  ]);
-$clientSecret = $paymentIntent->client_secret;
-    echo json_encode(['clientSecret' => $clientSecret, 'data' => 'jsondtb'.$data['amount'], 'error' => ""]);
-exit;
+    $paymentIntent = $stripe->paymentIntents->create([
+        'automatic_payment_methods' => ['enabled' => true],
+        'amount'                    => (int) ($data['amount']),
+        //'amount' => 1099,
+        'currency'                  => 'eur',
+    ]);
+    $clientSecret = $paymentIntent->client_secret;
+    echo json_encode(['clientSecret' => $clientSecret, 'data' => 'jsondtb' . $data['amount'], 'error' => ""]);
+    exit;
 
 } catch (\Stripe\Exception\ApiErrorException $e) {
-  http_response_code(400);
- 
+    http_response_code(400);
+
     echo json_encode(['clientSecret' => "", 'adata' => $json_data, 'error' => $e->getError()->message]);
     exit;
-    } catch (Exception $e) {
+} catch (Exception $e) {
     error_log($e);
     http_response_code(500);
     exit;
-    }
+}
