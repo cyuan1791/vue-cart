f1c2c5f2bbc2fa02265c2de6caa9aa055ccf3e33
Mark Yuan <mark@webasone.com>
2025-08-31 21:12:56 -0700

    udate

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,12 @@ import Home from "./pages/Home.vue";
 import Product from "./pages/Product.vue";
 import Cart from "./pages/Cart.vue";
 import Checkout from "./pages/Checkout.vue";
-import Return from "./pages/Return.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/product/:productId", component: Product },
   { path: "/cart", component: Cart },
   { path: "/checkout", component: Checkout },
-  { path: "/return", component: Return },
 ];
 
 const router = createRouter({
